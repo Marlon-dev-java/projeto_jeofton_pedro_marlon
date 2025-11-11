c8646f5b7a285afde550dff00cf83f10993229c9
Marlon-dev-java <marlondants20@gmail.com>
2025-11-11 13:02:17 -0300

    primeiro commit de marlon :arrow_up:

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro commit</title>
+</head>
+<body>
+    <?php
+        $bebida = $_GET['marca'];
+        //teste
+   
+        switch (strtolower($marca)) {
+         case ("toyota"):
+            echo "<p>A marca escolhida foi: $marca</p>";
+            break;
+         case ( "fiat"): 
+            echo "<p>A marca escolhida foi: $marca</p>";
+            break;
+         case ( "volvo"): 
+            echo "<p>A marca escolhida foi: $marca</p>";
+            break;
+         case ( "honda"): 
+            echo "<p>A marca escolhida foi: $marca</p>";
+            break;
+         default :
+            echo "<p.O cliente não escolheu nenhuma marca.</p>";
+        }
+    ?>
+    </body>
+</html>
